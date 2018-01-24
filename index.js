27bda29e7e19b8e5f84625c6ac14341acfbb21e7
Zacharoula 'Lulu' Morfogen <lulu@cipher-schema.net>
2018-01-24 00:12:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ var recipes = {prop: 1};
 
 function updateObjectWithKeyAndValue(object,key,value){
   return Object.assign({},object,object[key]=value);
+  // this is not mutating the object
 }
 
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
+  return Object.assign(object,object[key]=value);
+}
+
+destructivelyUpdateObjectWithKeyAndValue(recipes,'prop2',2);
