3ae3df37685089bdf71955e5006d1efdf1846c23
Zacharoula 'Lulu' Morfogen <lulu@cipher-schema.net>
2018-01-24 00:37:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+var recipes = {prop: 1};
+
+function updateObjectWithKeyAndValue(object,key,value){
+  var newObj = Object.assign({},object);
+  newObj[key] = value;
+  return newObj;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
+  return Object.assign(object,object[key]=value);
+}
+
+function deleteFromObjectByKey(object, key){
+  var newObj = Object.assign({},object);
+  delete newObj[key];
+  return newObj;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key];
+  return object;
+}
