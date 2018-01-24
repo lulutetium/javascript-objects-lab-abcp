d999212e5f431f8c5f68f6b29679ecfe5e9a4aea
Zacharoula 'Lulu' Morfogen <lulu@cipher-schema.net>
2018-01-24 00:19:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var recipes = {prop: 1};
 
+var newo
+
 function updateObjectWithKeyAndValue(object,key,value){
   return Object.assign({},object,object[key]=value);
   // this TEST IS FUCKED FIGURE OUT WHY DUMMY
