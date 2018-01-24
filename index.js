4fd1cf901e86075d2f72b57a703e172a3e1ab429
Zacharoula 'Lulu' Morfogen <lulu@cipher-schema.net>
2018-01-24 00:30:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
   return Object.assign(object,object[key]=value);
 }
 
+function deleteObjectByKey(object,key){
+  var newObj = Object.assign({},object);
+  delete newObj[key];
+  return newObj;
+}
